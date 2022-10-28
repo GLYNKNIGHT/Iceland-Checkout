b30c4c930a4660859b062eebd82f37450f09596a
glynk <glyn_knight@hotmail.com>
2022-10-28 10:59:06 +0100

    added json server as mock backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+const [basket, setBasket] = useState()
+
+
+useEffect(()=> {
+    fetch('http://localhost:8000/basket')
+    .then(res =>{
+      return res.json();
+    })
+    .then((data)=> {console.log(data); setBasket(data)});
+
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
